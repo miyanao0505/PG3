eea5ce8eb507f340ea3f2af39287beab8b01341b
miyanao <k023g0050@g.neec.ac.jp>
2024-09-30 12:57:05 +0900

    01_01_オーバーロードと関数のテンプレート_確認課題

--- main.cpp
+++ main.cpp
@@ -1,10 +1,28 @@
 ﻿#include <stdio.h>
+#include <string.h>
+
+template <typename T>
+T Min(T a, T b) {
+	return a < b ? a : b;
+}
+
+template<>
+char* Min<char*>(char* a, char* b) {
+	static char ans[] = "数字以外は代入できません";
+	return ans;
+}
 
 int main()
 {
-	char str[] = "自由";
+	int x = 5, y = 8;
+	float l = 1.5f, m = 3.8f;
+	double i = 0.8, k = 5.8;
+	char p[] = "3", q[] = "6";
 
-	printf("%s\n", str);
+	printf("%d\n", Min<int>(x, y));
+	printf("%f\n", Min<float>(l, m));
+	printf("%lf\n", Min<double>(i, k));
+	printf("%s\n", Min<char*>(p, q));
 
 	return 0;
 }
