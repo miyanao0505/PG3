c8fa7d1ee3520c9d18d468b8327b3e6818102b6a
miyanao <k023g0050@g.neec.ac.jp>
2024-10-28 11:43:16 +0900

    02_01_メンバ関数ポインタ_確認課題

--- main.cpp
+++ main.cpp
@@ -1,60 +1,15 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-#include <Windows.h>
-#include <functional>
+#include "script/Enemy.h"
 
-typedef void (*PFunc)(bool*);
-
-void DispResult(bool *ans) {
-	(*ans) ? printf("正解\n") : printf("不正解\n");
-}
-
-void setTimeout(int second) {
-	Sleep(second * 1000);
-}
-
-/// <summary>
-///  奇数か偶数か
-/// </summary>
-/// <param name="num">値</param>
-/// <returns>1:奇数, 2:偶数</returns>
-int isOddOrEven(int num) {
-	if (num % 2 == 1) {
-		return 1;
-	}
-	return 2;
-}
-
-bool isMatch(int input, int value) {
-	if (input == value) {
-		return true;
-	}
-	return false;
-}
-
-int main(int argc, const char *argv[])
+int main()
 {
-	std::function<void(int, int)> ans = [](int input, int result) {
-		bool ans = isMatch(input, isOddOrEven(result));
-		DispResult(&ans);
-	};
+	Enemy enemy;
 
-	// 現在時刻の情報で初期化
-	srand((unsigned int)time(NULL));
+	enemy.Initialize();
 
-	int result = rand() % 6 + 1;
-	int input = 0;
-
-	printf("スタート\n");
-	printf("サイコロの出目が半(奇数)か丁(偶数)か？\n");
-	printf("1：奇数, 2：偶数\n");
-	printf("->");
-	scanf_s("%d", &input);
-
-	setTimeout(3);
-
-	ans(input, result);
+	enemy.Update();
+	enemy.Update();
+	enemy.Update();
 
 	return 0;
 }
